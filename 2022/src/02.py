f26ceccbb5d1c2b4eff96620801b22e6784180e0
Guglielmo Cerri <cerriguglielmo@gmail.com>
2022-12-04 19:03:21 +0100

    solution for day 2

---
+++ 2022/src/02.py
@@ -0,0 +1,66 @@
+import lib.aoc as aoc
+from typing import Tuple
+
+score_dict  = { 'X':1, 'Y':2, 'Z':3}
+choose_dict = { 'A':'X', 'B':'Y', 'C':'Z' }
+game = { 
+    ('X','Y') : 6,
+    ('X','Z') : 0,
+    ('Y','X') : 0,
+    ('Y','Z') : 6,
+    ('Z','X') : 6,
+    ('Z','Y') : 0,
+    ('X','X') : 3,
+    ('Y','Y') : 3,
+    ('Z','Z') : 3,
+}
+
+lose = { 
+    'X' : 'Z',
+    'Y' : 'X',
+    'Z' : 'Y'
+}
+
+win = {
+    'X' : 'Y',
+    'Y' : 'Z',
+    'Z' : 'X'
+}
+
+@aoc.main('02', 1)
+def main1(indata: str) -> Tuple[int, int]:
+    data = indata.split('\n')
+    score = 0
+
+    for i in data:
+        c1, c2 = i.split()
+        c1 = choose_dict[c1]
+
+        score += 3 if c1 == c2 else game[ (c1, c2) ]
+        score += score_dict[c2]
+
+    return score
+
+@aoc.main('02', 2)
+def main2(indata: str) -> Tuple[int, int]:
+    data = indata.split('\n')
+    score = 0
+
+    for i in data:
+        c1, c2 = i.split()
+        c1 = choose_dict[c1]
+
+        match c2:
+            case 'X':
+                c3 = lose[c1]
+            case 'Y':
+                c3 = c1    
+            case 'Z':
+                c3 = win[c1]
+
+        score += score_dict[c3] + game[ (c1, c3) ]
+
+    return score
+
+if __name__ == "__main__":
+    main2()
