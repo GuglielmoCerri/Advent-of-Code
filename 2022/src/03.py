b8c212edeb5e45afec7865b175835cd40e050326
Guglielmo Cerri <guglielmo.cerri@cogentech.it>
2022-12-06 09:20:44 +0100

    day 3 - part 2 solution

--- 2022/src/03.py
+++ 2022/src/03.py
@@ -6,7 +6,7 @@ ALPHABET = list(string.ascii_lowercase)
 
 
 @aoc.main('03', 1)
-def main1(indata: str) -> Tuple[int, int]:
+def main_p1(indata: str) -> Tuple[int, int]:
     data = indata.split('\n')
     score = 0
 
@@ -25,15 +25,13 @@ def main1(indata: str) -> Tuple[int, int]:
     return score
 
 @aoc.main('03', 2)
-def main2(indata: str) -> Tuple[int, int]:
+def main_p2(indata: str) -> Tuple[int, int]:
     data = indata.split('\n')
     score = 0
 
-    for i in data:
-        len_ = len(i)
-        s1 = i[0: int(len_/2)]
-        s2 = i[int(len_/2): len_]
-        common = set(s1).intersection(set(s2))
+    for i in range(0, len(data), 3):
+        s1,s2,s3 = data[i:i+3]
+        common = set(s1) & set(s2) & set(s3)
         common = next(iter(common))
         val = ALPHABET.index(common.lower()) + 1
         if common.isupper():
@@ -44,4 +42,4 @@ def main2(indata: str) -> Tuple[int, int]:
     return score
 
 if __name__ == "__main__":
-    main1()
+    main_p2()
