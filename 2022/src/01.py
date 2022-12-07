e137f75f85399af18bebdc911669100292b4e6f4
Guglielmo Cerri <guglielmo.cerri@cogentech.it>
2022-12-07 12:57:43 +0100

    solved day 1 with nested list comprehension

--- 2022/src/01.py
+++ 2022/src/01.py
@@ -2,39 +2,21 @@ import lib.aoc as aoc
 from typing import Tuple
 
 @aoc.main('01', 1)
-def main1(indata: str) -> Tuple[int, int]:
-    total_calories = []
-    elve_calorie = []
+def main_p1(indata: str) -> Tuple[int, int]:
+    
+    elv_calories = [ sum([int(cal) for cal in calories.split('\n')]) for calories in indata.split('\n\n')]
+    elv_calories.sort(reverse=True)
 
-    for i in indata.split('\n'):
-        if i:
-            elve_calorie.append( int(i) )
-        else:
-            total_calories.append(elve_calorie)
-            elve_calorie = []
-            
-    calories = [sum(i) for i in total_calories]
-
-    return calories[0]
+    return elv_calories[0]
 
 
 @aoc.main('01', 2)
-def main2(indata: str) -> Tuple[int, int]:
-    total_calories = []
-    elve_calorie = []
+def main_p2(indata: str) -> Tuple[int, int]:
+    elv_calories = [ sum([int(cal) for cal in calories.split('\n')]) for calories in indata.split('\n\n')]
+    elv_calories.sort(reverse=True)
 
-    for i in indata.split('\n'):
-        if i:
-            elve_calorie.append( int(i) )
-        else:
-            total_calories.append(elve_calorie)
-            elve_calorie = []
-            
-    calories = [sum(i) for i in total_calories]
-    calories.sort(reverse=True)
-
-    return sum(calories[:3])
+    return sum(elv_calories[:3])
 
 if __name__ == "__main__":
-    main1()
-    main2()
+    main_p1()
+    main_p2()
