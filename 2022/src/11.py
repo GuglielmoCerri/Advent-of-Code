6f86d6a4c4bac1ba2163645b49314b3f79d5913d
Guglielmo Cerri <guglielmo.cerri@cogentech.it>
2022-12-13 11:23:35 +0100

    day 11 part 2 solution

--- 2022/src/11.py
+++ 2022/src/11.py
@@ -2,6 +2,8 @@ import lib.aoc as aoc
 import re 
 import operator
 
+ROUNDS_P1 = 20
+ROUNDS_P2 = 10000
 
 operators = {
     "+": operator.add,
@@ -9,7 +11,6 @@ operators = {
     "*": operator.mul,
     "/": operator.truediv,
 } 
-
 
 #-------------------------------------------------------------------------------
 class Monkey:
@@ -23,21 +24,26 @@ class Monkey:
         self.false_choice = false_choice
         self.__activity   = 0
         
-    def _computing_new_value(self, item):
-        if isinstance(self.operand, int):
-            return operators.get(self.operation)(item, self.operand)
-        else:
-            return operators.get(self.operation)(item, item)
+    def _reduce_size(self, item, lcd):
+        return item % lcd #if self.part_2 else item
     
+    def _computing_new_value(self, item, lcd):
+        if isinstance(self.operand, int):
+            new_item = operators.get(self.operation)(item, self.operand)
+        else:
+            new_item = operators.get(self.operation)(item, item)
+    
+        return self._reduce_size(new_item, lcd)
+        
     def _compute_choice(self, val):
         return self.true_choice if val % self.divisible == 0 else self.false_choice
     
-    def inspecting(self):
+    def inspecting(self, lcd):
         self._calculate_activity()
         tmp_list   = self.items[:]
         self.items = []
         return \
-            [(self._compute_choice(self._computing_new_value(item)),self._computing_new_value(item)) for item in tmp_list]
+            [(self._compute_choice(self._computing_new_value(item, lcd)),self._computing_new_value(item, lcd)) for item in tmp_list]
     
     def add_item(self, item):
         self.items.append(item)
@@ -59,27 +65,34 @@ def parser(l:list):
     
     return name, items, expression, divisible, true_choice, false_choice
 
+#-------------------------------------------------------------------------------
+def monkey_initialization(indata):
+    monkeys = dict()
+    lcd = 1  # lowest common denominator
+    for i in range(0, len(indata), 6):
+        name, items, expression, divisible, true_choice, false_choice = parser(indata[i:i+6])
+        monkey = Monkey(items, expression, divisible, true_choice, false_choice)
+        monkeys[name] = monkey
+        lcd *= monkey.divisible
+        
+    return monkeys, lcd
+    
 #-------------------------------------------------------------------------------    
 @aoc.main('11', 1)
 def main_p1(indata: str) -> int:
     indata = indata.split('\n')
     indata = [i for i in indata if i]
 
-    # Initialize monkeys
-    monkeys = dict()
-    for i in range(0, len(indata), 6):
-        name, items, expression, divisible, true_choice, false_choice = parser(indata[i:i+6])
-        monkey = Monkey(items, expression, divisible, true_choice, false_choice)
-        monkeys[name] = monkey
-
+    monkeys, lcd = monkey_initialization(indata)
+   
     _ = [ [ [monkeys[new_item[0]].add_item(new_item[1]) 
-            for new_item in monkeys[monkey].inspecting() ] \
+            for new_item in monkeys[monkey].inspecting(lcd) ] \
             for monkey in monkeys] \
-            for _ in range(20)]
+            for _ in range(ROUNDS_P1)]
 
     business = [monkeys[monkey].get_activity() for monkey in monkeys]
     business.sort(reverse=True)
-
+    
     return business[0] * business[1]
 
 #-------------------------------------------------------------------------------    
@@ -88,17 +101,12 @@ def main_p2(indata: str) -> str:
     indata = indata.split('\n')
     indata = [i for i in indata if i]
 
-    # Initialize monkeys
-    monkeys = dict()
-    for i in range(0, len(indata), 6):
-        name, items, expression, divisible, true_choice, false_choice = parser(indata[i:i+6])
-        monkey = Monkey(items, expression, divisible, true_choice, false_choice)
-        monkeys[name] = monkey
+    monkeys, lcd = monkey_initialization(indata)
 
     _ = [ [ [monkeys[new_item[0]].add_item(new_item[1]) 
-            for new_item in monkeys[monkey].inspecting() ] \
+            for new_item in monkeys[monkey].inspecting(lcd) ] \
             for monkey in monkeys] \
-            for _ in range(10000)]
+            for _ in range(ROUNDS_P2)]
 
     business = [monkeys[monkey].get_activity() for monkey in monkeys]
     business.sort(reverse=True)
@@ -106,5 +114,5 @@ def main_p2(indata: str) -> str:
     return business[0] * business[1]
 
 if __name__ == "__main__":
-    #main_p1()
+    main_p1()
     main_p2()
