bc81a5a0ab9487324fc39561a9bd492b50d397fa
Guglielmo Cerri <guglielmo.cerri@avvale.com>
2024-12-05 09:21:05 +0100

    Day 5 solution

---
+++ 2024/src/05.py
@@ -0,0 +1,73 @@
+from collections import defaultdict, deque
+
+from lib.aoc import aoc
+
+def is_update_valid(update, rules):
+        index_map = {page: idx for idx, page in enumerate(update)}
+        for x, y in rules:
+            if x in index_map and y in index_map:
+                if index_map[x] > index_map[y]:
+                    return False
+        return True
+
+def get_middle_page(update):
+    n = len(update)
+    return update[n // 2]
+
+def topological_sort(update, rules):
+        graph = defaultdict(list)
+        in_degree = defaultdict(int)
+        update_set = set(update)
+
+        for x, y in rules:
+            if x in update_set and y in update_set:
+                graph[x].append(y)
+                in_degree[y] += 1
+                in_degree.setdefault(x, 0)
+
+        queue = deque([node for node in update if in_degree[node] == 0])
+        sorted_update = []
+
+        while queue:
+            node = queue.popleft()
+            sorted_update.append(node)
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return sorted_update
+
+@aoc('05', 1)
+def p1(indata: str) -> int:
+    rules_section, updates_section = indata.strip().split("\n\n")
+    rules = [tuple(map(int, line.split('|'))) for line in rules_section.splitlines()]
+    updates = [list(map(int, line.split(','))) for line in updates_section.splitlines()]
+
+    res = 0
+
+    for update in updates:
+        if is_update_valid(update, rules):
+            res += get_middle_page(update)
+
+    return res
+
+@aoc('05', 2)
+def p2(indata: str) -> int:
+
+    rules_section, updates_section = indata.strip().split("\n\n")
+    rules = [tuple(map(int, line.split('|'))) for line in rules_section.splitlines()]
+    updates = [list(map(int, line.split(','))) for line in updates_section.splitlines()]
+
+    res = 0
+
+    for update in updates:
+        if not is_update_valid(update, rules):
+            corrected_update = topological_sort(update, rules)
+            res += get_middle_page(corrected_update)
+
+    return res
+
+if __name__ == "__main__":
+    p1()
+    p2()
